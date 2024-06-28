d76ecf358b885b083586ea532f4198bd5fada723
Bobby <sogo.rush@gmail.com>
2024-06-28 10:00:26 -0700

    cleaned the data for duplicate years

--- Project3.r
+++ Project3.r
@@ -1,6 +1,6 @@
 climateData <- read.csv("C:\\Users\\sougou.tojima\\Documents\\ClimateData.csv")
 print(climateData)
-print(summary(climateData))
+#print(summary(climateData))
 #Printing a data will just print the whole excel spreadsheet on the terminal.
 #Printing a summary prints all the important data informations such as the min/max or the mean/medians.
 
@@ -19,6 +19,11 @@ print(i);
 #animals[2] <- "german shepard"
 animals <- c("cat", "dog", "giraffe", "bunny")
 animals[2] <- "german shepard"
-cat(animals)
+#cat(animals)
 #b)
 #They represent the columns (the vertical part)
+
+yearTF <- duplicated(climateData$"Year")
+print(which(yearTF == TRUE))
+climateData <- climateData[-c(19, 33, 67), ]
+print(climateData)
